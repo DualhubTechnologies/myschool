563c74b401bf597dc3c5951bc33078c086cff49a
adrian <adrianmugizi8@gmail.com>
2026-01-12 13:09:50 +0300

    updates

--- myschool/settings.py
+++ myschool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&23djb6w^1g6sxz(%&muroqai^&ol0b2uy7!l9p1ahmk#rrzsh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
